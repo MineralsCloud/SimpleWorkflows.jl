637a562a2cbae658e9cce53ceb411a3f2651e28a
singularitti <singularitti@outlook.com>
2020-09-10 02:41:55 -0400

    Copy code from Express.jl
    https://raw.githubusercontent.com/MineralsCloud/Express.jl/master/src/Jobs.jl?token=AGAGOBLYS2SPHIYP47SGMIS7MMCSM

--- src/SimpleWorkflow.jl
+++ src/SimpleWorkflow.jl
@@ -1,5 +1,67 @@
 module SimpleWorkflow
 
-# Write your package code here.
+using Dates: unix2datetime
+using Distributed: Future, @spawn
+using UUIDs: UUID, uuid4
+
+abstract type JobStatus end
+struct Running <: JobStatus end
+abstract type Finished <: JobStatus end
+struct Succeeded <: Finished end
+struct Failed <: Finished end
+
+mutable struct Timer
+    start::Float64
+    stop::Float64
+    Timer() = new()
+end
+
+mutable struct JobRef
+    ref::Future
+    status::JobStatus
+    JobRef() = new()
+end
+
+abstract type Job end
+abstract type AtomicJob <: Job end
+struct ExternalAtomicJob
+    cmd
+    name::String
+    id::UUID
+    timer::Timer
+    logger
+    ExternalAtomicJob(cmd, name = "Unnamed") = new(cmd, name, uuid4(), Timer(), "")
+end
+
+function _launch(cmd::Base.AbstractCmd)
+    x = ExternalAtomicJob(cmd)
+    x.ref.ref = @spawn begin
+        x.timer.start = time()
+        ref = try
+            run(cmd; wait = true)  # Must wait
+        catch e
+            e
+        finally
+            x.timer.stop = time()
+        end
+        if ref isa Exception  # Include all cases?
+            if ref isa InterruptException
+                x.ref.status = Succeeded()
+            else
+                x.ref.status = Failed()
+            end
+        else
+            x.ref.status = Succeeded()
+        end
+        ref
+    end
+    return x
+end
+
+starttime(x::AtomicJob) = unix2datetime(x.timer.start)
+
+stoptime(x::AtomicJob) = isrunning(x) ? nothing : x.timer.stop
+
+timecost(x::AtomicJob) = isrunning(x) ? time() - starttime(x) : stoptime(x) - starttime(x)
 
 end
