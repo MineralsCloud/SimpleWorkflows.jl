fc0c2474518d057236c070f36952351a95b1c38c
singularitti <singularitti@outlook.com>
2020-10-16 20:37:43 -0400

    Closes #9

--- src/graph.jl
+++ src/graph.jl
@@ -13,7 +13,7 @@ using LightGraphs:
     dst
 using BangBang: push!!, pushfirst!!, append!!
 
-export Workflow, eachjob, ←, →, ∥
+export Workflow, eachjob, chain, backchain, parallel, ←, →, ∥
 
 struct Workflow
     graph::DiGraph{Int}
@@ -37,34 +37,34 @@ Base.getindex(w::Workflow, i::Integer) = TieInPoint(w, UInt(i))
 Base.firstindex(w::Workflow) = 1
 Base.lastindex(w::Workflow) = nv(w.graph)
 
-→(a::Job, b::Job) = Workflow(DiGraph(2, 1), (a, b))
-function →(a::Job, b::Job, c::Job, xs::Job...)  # See https://github.com/JuliaLang/julia/blob/be54a6c/base/operators.jl#L540
+chain(a::Job, b::Job) = Workflow(DiGraph(2, 1), (a, b))
+function chain(a::Job, b::Job, c::Job, xs::Job...)  # See https://github.com/JuliaLang/julia/blob/be54a6c/base/operators.jl#L540
     n = length(xs)
     g = DiGraph(3 + n)
     map(i -> add_edge!(g, i, i + 1), 1:(2+n))
     return Workflow(g, (a, b, c, xs...))
 end
-function →(wi::TieInPoint, j::Job)
+function chain(wi::TieInPoint, j::Job)
     add_vertex!(wi.workflow.graph)
     add_edge!(wi.workflow.graph, wi.index, nv(wi.workflow.graph))
     return Workflow(wi.workflow.graph, push!!(wi.workflow.nodes, j))
 end
-function →(j::Job, wi::TieInPoint)
+function chain(j::Job, wi::TieInPoint)
     g = DiGraph(1)
     h = g ⊕ wi.workflow.graph
     add_edge!(h, 1, wi.index + 1)
     return Workflow(h, pushfirst!!(wi.workflow.nodes, j))
 end
-function →(a::TieInPoint, b::TieInPoint)
+function chain(a::TieInPoint, b::TieInPoint)
     g = a.workflow.graph ⊕ b.workflow.graph
     add_edge!(g, a.index, b.index + nv(a.workflow.graph))
     return Workflow(g, append!!(a.workflow.nodes, b.workflow.nodes))
 end
 
-←(a::Union{Job,TieInPoint}, b::Union{Job,TieInPoint}) = →(b, a)
-←(c::Job, b::Job, a::Job, xs::Job...) = →(xs..., a, b, c)
+backchain(a::Union{Job,TieInPoint}, b::Union{Job,TieInPoint}) = chain(b, a)
+backchain(c::Job, b::Job, a::Job, xs::Job...) = chain(xs..., a, b, c)
 
-function ∥(a::Job, b::Job, xs::Job...)
+function parallel(a::Job, b::Job, xs::Job...)
     g = DiGraph(4 + length(xs))
     n = nv(g)
     for i in 2:(n-1)
@@ -73,7 +73,7 @@ function ∥(a::Job, b::Job, xs::Job...)
     end
     return Workflow(g, (EmptyJob(), a, b, xs..., EmptyJob()))
 end
-function ∥(w::TieInPoint, b::Job)
+function parallel(w::TieInPoint, b::Job)
     @assert length(inneighbors(w.workflow.graph, w.index)) == 1
     p = inneighbors(w.workflow.graph, w.index)
     g = w.workflow.graph
@@ -81,20 +81,24 @@ function ∥(w::TieInPoint, b::Job)
     add_edge!(g, only(p), nv(g))
     return Workflow(g, push!!(w.workflow.nodes, b))
 end
-∥(j::Job, w::TieInPoint) = ∥(w, j)
-function ∥(a::TieInPoint, b::TieInPoint)
+parallel(j::Job, w::TieInPoint) = parallel(w, j)
+function parallel(a::TieInPoint, b::TieInPoint)
     g = DiGraph(1) ⊕ a.workflow.graph ⊕ b.workflow.graph
     add_edge!(g, 1, a.index + 1)
     add_edge!(g, 1, b.index + 1 + nv(a.workflow.graph))
     return Workflow(g, (EmptyJob(), a.workflow.nodes..., b.workflow.nodes...))
 end
 
-for op in (:→, :←, :∥)
+for op in (:chain, :backchain, :parallel)
     @eval begin
         ($op)(a::TieInPoint, b::TieInPoint, c::TieInPoint, xs::TieInPoint...) =
             Base.afoldl(($op), a, b, c, xs...)
     end
 end
+
+const → = chain
+const ← = backchain
+const ∥ = parallel
 
 eachjob(w::Workflow) = (w.nodes[i] for i in vertices(w.graph))
 
