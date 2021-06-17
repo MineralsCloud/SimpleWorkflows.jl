a83f272f67ae5c055d42f9443c966d04cb62f2ae
Documenter.jl <documenter@juliadocs.github.io>
2021-06-17 20:29:23 +0000

    build based on 4a4553e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleWorkflow","category":"page"},{"location":"#SimpleWorkflow","page":"Home","title":"SimpleWorkflow","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleWorkflow]","category":"page"}]
+}
