MineralsCloud/SimpleWorkflows.jl

commit 7dd30317a7bffaacb37cda331041e6f6a2b91f20

Fix operations on rows in `_register!` since `id` is now unique

src/SimpleWorkflow.jl
@@ -119,10 +119,10 @@ function _register!(job::AtomicJob)
     )
     result = _run!(job)
     # Update JOB_REGISTRY
-    rows = query(job.id)
-    rows[:, :status] .= job.status
-    rows[:, :stop_time] .= job.stop_time
-    rows[:, :duration] .= job.stop_time - job.start_time
+    row = query(job.id)
+    row.status = job.status
+    row.stop_time = job.stop_time
+    row.duration = job.stop_time - job.start_time
     # Return the result
     return result
 end

commit 8b661d6f83249347aa83266573924cb2f0035b5c

Fix `_register!` result since `query` returns a `DataFrame`, not a `DataFrameRow`
this bug was introduced in commit
[7dd3031](https://github.com/MineralsCloud/SimpleWorkflow.jl/commit/7dd30317a7bffaacb37cda331041e6f6a2b91f20)
in #40

src/SimpleWorkflow.jl
@@ -118,7 +118,7 @@ function _register!(job::AtomicJob)
     )
     result = _run!(job)
     # Update JOB_REGISTRY
-    row = query(job.id)
+    row = first(query(job.id))  # `query` returns a `DataFrame`, not a `DataFrameRow`
     row.status = job.status
     row.stop_time = job.stop_time
     row.duration = job.stop_time - job.start_time