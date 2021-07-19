20e6b328e5806f2e7918a1522945437f109df11e
Reno <25192197+singularitti@users.noreply.github.com>
2021-07-19 00:41:59 -0400

    Add methods `dependencies` & `→` for `Job`s

--- src/graph.jl
+++ src/graph.jl
@@ -17,6 +17,26 @@ using MetaGraphs: MetaGraph, set_prop!
 
 const DEPENDENCIES = Dict{Job,Vector{AtomicJob}}()
 
+dependencies(job::Job) = get(DEPENDENCIES, job, AtomicJob[])
+
+function →(a::Job, b::Job)
+    if a == b
+        throw(ArgumentError("a job cannot have itself as a dependency!"))
+    else
+        if haskey(DEPENDENCIES, b)
+            push!(DEPENDENCIES[b], a)
+        else
+            push!(DEPENDENCIES, b => [a])  # Initialization
+        end
+        return
+    end
+end
+function →(a::Job, b::Job, c::Job, xs::Job...)  # See https://github.com/JuliaLang/julia/blob/be54a6c/base/operators.jl#L540
+    foreach(zip((a, b, xs[1:end-1]...), (b, c, xs...))) do x, y
+        x → y
+    end
+end
+
 function ⊕(g::AbstractGraph, b::AbstractGraph)
     a = copy(g)
     add_vertices!(a, nv(b))
