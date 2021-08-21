5e040cac7c273f64a7527013bfa5202e2e6fbef6
Reno <25192197+singularitti@users.noreply.github.com>
2021-08-21 02:56:44 -0400

    Fix tests

--- test/workflow.jl
+++ test/workflow.jl
@@ -5,8 +5,8 @@ k = AtomicJob(() -> sleep(5))
 l = AtomicJob(`sleep 3`)
 m = AtomicJob(() -> sin(1))
 n = AtomicJob(`pwd` & `sleep 3`)
-j → l → k → n → m
-j → k
-k → m
+j ▷ l ▷ k ▷ n ▷ m
+j ▷ k
+k ▷ m
 w = Workflow(l, k, j, m, n)
 @test w.nodes == Workflow(k, j, l, n, m).nodes == Workflow(k, l, m, n, j).nodes
