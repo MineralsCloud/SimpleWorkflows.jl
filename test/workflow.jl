4046cadc3379d76248eb88132b13c0e9c90dd6e0
Reno <25192197+singularitti@users.noreply.github.com>
2021-10-04 03:13:06 -0400

    Add more tests

--- test/workflow.jl
+++ test/workflow.jl
@@ -12,3 +12,8 @@ k ▷ m
 w = Workflow(l, k, j, m, n)
 @test w.jobs == Workflow(k, j, l, n, m).jobs == Workflow(k, l, m, n, j).jobs
 run!(w)
+@test something(getresult(j)) isa Base.Process
+@test something(getresult(k)) === nothing
+@test something(getresult(l)) isa Base.Process
+@test something(getresult(m)) == 0.8414709848078965
+@test something(getresult(n)) isa Base.ProcessChain
