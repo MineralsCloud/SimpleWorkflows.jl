9a3e5479c423b0792597682a491c49e596543266
singularitti <singularitti@outlook.com>
2022-11-24 07:13:58 -0500

    Fix function args in "Test running a `Workflow`" after https://github.com/MineralsCloud/EasyJobsBase.jl/pull/10

--- test/workflow.jl
+++ test/workflow.jl
@@ -31,10 +31,10 @@ using SimpleWorkflows: Workflow, AutosaveWorkflow
         cos(x)
         return run(`pwd` & `ls`)
     end
-    i = Job(Thunk(f₁, ()); username="me", name="i")
+    i = Job(Thunk(f₁); username="me", name="i")
     j = Job(Thunk(f₂, 3); username="he", name="j")
     k = Job(Thunk(f₃, 6); name="k")
-    l = Job(Thunk(f₄, ()); name="l", username="me")
+    l = Job(Thunk(f₄); name="l", username="me")
     m = Job(Thunk(f₅, 3, 1); name="m")
     n = Job(Thunk(f₆, 1; x=3); username="she", name="n")
     i → l
