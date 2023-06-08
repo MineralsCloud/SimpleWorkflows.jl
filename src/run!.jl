a7714841a1b59393bb654a5df865b4e1f3220a7b
singularitti <singularitti@outlook.com>
2023-06-07 23:07:01 -0400

    Add comments to `run_kahn_algo!`

--- src/run!.jl
+++ src/run!.jl
@@ -36,7 +36,15 @@ function run_copy!(wf; δt)  # Do not export!
     return wf
 end
 
+# This function `run_kahn_algo!` is an implementation of Kahn's algorithm for job scheduling.
+# `wf` is a workflow that will be saved after each job execution.
+# `jobs` is a list of jobs to be executed.
+# `graph` is a directed acyclic graph representing dependencies between jobs.
+# `execs` is a list of executors that can run the jobs.
 function run_kahn_algo!(wf, jobs, graph, execs)  # Do not export!
+    # Check if `jobs` is empty and if there are no vertices in the `graph`.
+    # This is the base case of the recursion, if there are no jobs left and no vertices in the graph,
+    # the function will stop its execution.
     if isempty(jobs) && iszero(nv(graph))  # Stopping criterion
         return nothing
     elseif isempty(jobs) && !iszero(nv(graph))
@@ -44,16 +52,26 @@ function run_kahn_algo!(wf, jobs, graph, execs)  # Do not export!
     elseif !isempty(jobs) && iszero(nv(graph))
         throw(ArgumentError("`graph` is empty but `jobs` is not!"))
     else
+        # Find all vertices with zero in-degree in the graph, these vertices have no prerequisites
+        # and can be executed immediately. They are put in a queue.
         queue = findall(iszero, indegree(graph))
+        # For each executor in the `queue`, start the job execution.
+        # The `@sync` macro ensures that the main program waits until all async blocks are done.
         @sync for exec in execs[queue]
+            # Run the jobs with no prerequisites in parallel since they are in the same level.
             @async begin
                 start!(exec)
                 wait(exec)
                 save(wf)
             end
         end
+        # Remove the vertices corresponding to the executed jobs from the graph.
+        # This also changes the indegree of the remaining vertices.
         rem_vertices!(graph, queue; keep_order=true)
+        # Remove the executed jobs from the jobs list.
         deleteat!(jobs, queue)
+        # Recursively call the `run_kahn_algo!` with the updated jobs list and graph.
+        # This will continue the execution with the remaining jobs that are now without prerequisites.
         return run_kahn_algo!(wf, jobs, graph, execs)
     end
 end
