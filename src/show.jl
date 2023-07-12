585c07cbc0064d8ef253f94847203e45986b8b8e
singularitti <singularitti@outlook.com>
2023-07-12 02:46:49 -0400

    Fix `Base.show(io::IO, wf::Workflow)`

--- src/show.jl
+++ src/show.jl
@@ -1,42 +1,47 @@
 using Dates: format
 using EasyJobsBase: ispending, isrunning, starttimeof, endtimeof, timecostof, printf
+using Graphs: ne
 
+# See https://docs.julialang.org/en/v1/manual/types/#man-custom-pretty-printing
 function Base.show(io::IO, wf::Workflow)
-    if get(io, :compact, false) || get(io, :typeinfo, nothing) == typeof(wf)
-        Base.show_default(IOContext(io, :limit => true), wf)  # From https://github.com/mauro3/Parameters.jl/blob/ecbf8df/src/Parameters.jl#L556
+    if get(io, :compact, false)
+        print(IOContext(io, :limit => true, :compact => true), summary(wf))
     else
-        println(io, summary(wf))
-        println(io, " jobs:")
-        for (i, job) in enumerate(wf.jobs)
-            println(io, "  (", i, ") ", "id: ", job.id)
-            if !isempty(job.description)
-                print(io, ' '^6, "description: ")
-                show(io, job.description)
-                println(io)
-            end
-            print(io, ' '^6, "core: ")
-            printf(io, job.core)
-            print(io, '\n', ' '^6, "status: ")
-            printstyled(io, getstatus(job); bold=true)
-            if !ispending(job)
-                print(
-                    io,
-                    '\n',
-                    ' '^6,
-                    "from: ",
-                    format(starttimeof(job), "dd-u-YYYY HH:MM:SS.s"),
-                    '\n',
-                    ' '^6,
-                    "to: ",
-                )
-                if isrunning(job)
-                    print(io, "still running...")
-                else
-                    println(io, format(endtimeof(job), "dd-u-YYYY HH:MM:SS.s"))
-                    print(io, ' '^6, "uses: ", timecostof(job))
-                end
-            end
+        njobs, nedges = nv(wf.graph), ne(wf.graph)
+        print(io, summary(wf), '(', njobs, " jobs, ", nedges, " edges)")
+    end
+end
+function Base.show(io::IO, ::MIME"text/plain", wf::Workflow)
+    println(io, summary(wf))
+    for (i, job) in enumerate(wf.jobs)
+        println(io, " [", i, "] ", "id: ", job.id)
+        if !isempty(job.description)
+            print(io, ' '^5, "description: ")
+            show(io, job.description)
             println(io)
         end
+        print(io, ' '^5, "core: ")
+        printf(io, job.core)
+        print(io, '\n', ' '^5, "status: ")
+        printstyled(io, getstatus(job); bold=true)
+        if !ispending(job)
+            print(
+                io,
+                '\n',
+                ' '^5,
+                "from: ",
+                format(starttimeof(job), "dd-u-YYYY HH:MM:SS.s"),
+                '\n',
+                ' '^5,
+                "to: ",
+            )
+            if isrunning(job)
+                print(io, "still running...")
+            else
+                println(io, format(endtimeof(job), "dd-u-YYYY HH:MM:SS.s"))
+                print(io, ' '^5, "uses: ", timecostof(job))
+            end
+        end
+        println(io)
     end
 end
