524c7e2db251485d1aaac87f1d2d78b6c3f6f5b6
singularitti <25192197+singularitti@users.noreply.github.com>
2026-02-28 00:06:17 +0000

    Format .jl files

--- src/operations.jl
+++ src/operations.jl
@@ -3,8 +3,9 @@ import EasyJobsBase: chain!, →, ←
 for (func, (op₁, op₂)) in zip((:chain!,), ((:→, :←),))
     @eval begin
         $func(x::AbstractWorkflow, y::AbstractWorkflow) = $func(last(x), first(y))
-        $func(x::AbstractWorkflow, y::AbstractWorkflow, z::AbstractWorkflow...) =
-            foldr($func, (x, y, z...))
+        $func(x::AbstractWorkflow, y::AbstractWorkflow, z::AbstractWorkflow...) = foldr(
+            $func, (x, y, z...)
+        )
         $op₁(x::AbstractWorkflow, y::AbstractWorkflow) = $func(x, y)
         $op₂(y::AbstractWorkflow, x::AbstractWorkflow) = $op₁(x, y)
     end
